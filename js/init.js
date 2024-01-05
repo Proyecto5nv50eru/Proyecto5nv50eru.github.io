c1791fd8b6bdee8d5b6d1737114a4643b09f4728
Proyecto5nv50eru <155572398+Proyecto5nv50eru@users.noreply.github.com>
2024-01-05 11:58:00 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
+  apiKey: "AIzaSyC218U2CzU4D0Tc_tkPC32e1pTolEr7NF4",
+    authDomain: "proyecto-final-c77b2.firebaseapp.com",
+    projectId: "proyecto-final-c77b2",
+    storageBucket: "proyecto-final-c77b2.appspot.com",
+    messagingSenderId: "845154975981",
+    appId: "1:845154975981:web:73a05fd94885efe44a8ce4",
+    measurementId: "G-NS9F6MNG39"
     });
